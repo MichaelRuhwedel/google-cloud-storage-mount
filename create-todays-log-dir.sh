c0c1728d4e3062810dcdf58a92292abbde2a90cf
Michael Ruhwedel <mruhwedel@small-improvements.com>
2015-09-13 23:19:37 +0200

    fix(mkdirs): make dirs readable for logstash

--- create-todays-log-dir.sh
+++ create-todays-log-dir.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mkdir -p /mnt/bucket/appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`/`date +"%d"`
+mkdir -m 755 -p /mnt/bucket/appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`/`date +"%d"`
