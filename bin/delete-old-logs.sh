97898d4fd125fac89dfb01aa47387b98bc4c9a19
Michael Ruhwedel <mruhwedel@small-improvements.com>
2015-12-08 16:13:15 +0100

    feat(sync): sync the whole month (to compensate for downtime)

--- bin/delete-old-logs.sh
+++ bin/delete-old-logs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find /mnt/logfiles_incoming -daystart -mtime +2 -exec rm {} \;
+find /mnt/logfiles_incoming -daystart -mtime +30 -exec rm -r {} \;
--- bin/sync-logs.sh
+++ bin/sync-logs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-TODAYS_LOGS=bucket/appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`/`date +"%d"`
-rsync --ignore-existing -r --chmod=D755,F644 /mnt/$TODAYS_LOGS/* /mnt/logfiles_incoming/$TODAYS_LOGS/
+RECENT_LOGS=bucket/appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`
+rsync --ignore-existing -r --chmod=D755,F644 /mnt/$RECENT_LOGS/* /mnt/logfiles_incoming/$RECENT_LOGS/
