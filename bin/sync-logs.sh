b7f48b9e86eb62f23ed5324cef98b9bcb7cb70ea
Michael Ruhwedel <mruhwedel@small-improvements.com>
2015-12-08 16:41:02 +0100

    fix(sync): create dirs befor we sync

--- bin/sync-logs.sh
+++ bin/sync-logs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-RECENT_LOGS=bucket/appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`
-rsync --ignore-existing -r --chmod=D755,F644 /mnt/$RECENT_LOGS/* /mnt/logfiles_incoming/$RECENT_LOGS/
+RECENT_LOGS=appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`
+
+SRC=/mnt/bucket/$RECENT_LOGS/*
+DST=/mnt/logfiles_incoming/$RECENT_LOGS
+
+mkdir -p $DST
+rsync --ignore-existing -r --chmod=D755,F644 $SRC $DST
