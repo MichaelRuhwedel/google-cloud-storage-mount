8f3704446eeba7e67a136266bd7ac7dcc3ccd818
k-lange <mail@klange.org>
2015-12-10 13:27:49 +0100

    feat(sync-logs): use file lock to prevent simultaneous execution

--- bin/sync-logs.sh
+++ bin/sync-logs.sh
@@ -4,5 +4,4 @@ RECENT_LOGS=appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`
 SRC=/mnt/bucket/$RECENT_LOGS/*
 DST=/mnt/logfiles_incoming/$RECENT_LOGS
 
-mkdir -p $DST
-rsync --ignore-existing -r --chmod=D755,F644 $SRC $DST
+flock -n /tmp/sync-logs.lock -c "mkdir -p $DST && rsync --ignore-existing -r --chmod=D755,F644 $SRC $DST"
