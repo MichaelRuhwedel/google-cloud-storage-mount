3274f561fd66196ae86b64688d6108bf814473c6
Michael Ruhwedel <mruhwedel@small-improvements.com>
2016-02-12 17:59:43 +0100

    feat(cloud-storage-logs): sync entire logs logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-CURRENT_MONTH_LOGS=appengine.googleapis.com/request_log/`date +"%Y"`/`date +"%m"`
+LOGS=appengine.googleapis.com/request_log
 
-SRC=gs://$BUCKET/$CURRENT_MONTH_LOGS
-DST=/mnt/logfiles_incoming/$CURRENT_MONTH_LOGS
+SRC=gs://$BUCKET/$LOGS
+DST=/mnt/logfiles_incoming/$LOGS
 
 echo syncing logs from $SRC to $DST
 
 sed -i s/PROJECT_ID/$PROJECT_ID/ ~/.boto
+
+mkdir -p $DST
 
 ./gsutil -m rsync -r $SRC $DST
