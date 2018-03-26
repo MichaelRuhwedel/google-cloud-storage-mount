443a381bc2e2aa1329fdb1a1919c8922d625ad18
Michael Ruhwedel <mruhwedel@small-improvements.com>
2018-03-26 13:32:18 +0200

    refactor(entry-point) better express intent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -34,5 +34,5 @@ sed -i s/PROJECT_ID/$PROJECT_ID/ ~/.boto
 mkdir -p $DST
 gsutil -m rsync -r $SRC $DST
 
- #gsutil will return non zero code when there is nothing to sync -> please check errors for logs
-true
+#gsutil will return non zero code when there is nothing to sync -> please check errors for logs
+exit 0
